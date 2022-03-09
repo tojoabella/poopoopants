b71110244bb7f3eac4115da1bb3530f229db4922
tojoabella <terricabella@gmail.com>
2022-03-09 12:41:11 -1000

    refactor routes for deployment

---
+++ server/api/lessons.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+router = express.Router();
+
+const monk = require('monk');
+const db = monk(process.env.MONGO_URI || 'localhost:27017/poopoopants');
+
+
+router.get('/', (req, res) => {
+    const lessons = db.get('lessons');
+    lessons
+        .find()
+        .then(lessons => {
+            res.json(lessons);
+        });
+});
+
+router.post('/', (req, res) => {
+    const lesson = {
+        chapter_no: req.body.chapter_no.toString(),
+        lesson_no: req.body.lesson_no.toString(),
+        details: req.body.details.toString(),
+        particles: req.body.particles.toString(),
+        created: new Date()
+    };
+    
+   const lessons = db.get('lessons');
+   lessons.insert(lesson)
+        .then((created_lesson) => {
+            res.json(created_lesson) /*return the created lesson*/
+        }).catch((err) => {
+            // An error happened while inserting
+        });
+});
+
+router.put('/', (req, res) => {
+
+})
+
+router.delete('/', (req, res) => {
+    const lessons = db.get('lessons');
+    lessons.drop()
+    .then(lessons => {
+        res.json(lessons);
+    });
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,59 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const monk = require('monk');
-const dotenv = require('dotenv');
 
-dotenv.config();
+const lessons_routes = require('./api/lessons')
+
+/*const dotenv = require('dotenv');
+dotenv.config();*/
 
 const app = express();
 app.use(cors()); 
 app.use(express.json());
+
+app.use('/lessons', lessons_routes)
+
 const db = monk(process.env.MONGO_URI || 'localhost:27017/poopoopants');
 db.then(() => {
     console.log('Connected correctly to server')
   })
+
 app.get('/', (req, res) => {
     res.json("the poopoopants api");
 });
 
-app.get('/lessons', (req, res) => {
-    const lessons = db.get('lessons');
-    lessons
-        .find()
-        .then(lessons => {
-            res.json(lessons);
-        });
-});
-
-app.post('/lessons', (req, res) => {
-    const lesson = {
-        chapter_no: req.body.chapter_no.toString(),
-        lesson_no: req.body.lesson_no.toString(),
-        details: req.body.details.toString(),
-        particles: req.body.particles.toString(),
-        created: new Date()
-    };
-    
-   const lessons = db.get('lessons');
-   lessons.insert(lesson)
-        .then((created_lesson) => {
-            res.json(created_lesson) /*return the created lesson*/
-        }).catch((err) => {
-            // An error happened while inserting
-        });
-});
-
-app.put('/lessons', (req, res) => {
-
-})
-
-app.delete('/lessons', (req, res) => {
-    const lessons = db.get('lessons');
-    lessons.drop()
-    .then(lessons => {
-        res.json(lessons);
-    });
-});
 
 
 app.listen(6969, () => {
