b5a6ab2613951c5168fb0ec1ccd6e7110fef088e
tojoabella <terricabella@gmail.com>
2022-03-09 11:52:47 -1000

    db over theinternet. delete lesson

--- client/client.js
+++ client/client.js
@@ -1,32 +1,69 @@
-const form = document.getElementById('add_new_lesson_form');
 
-form.addEventListener('submit', (event) => {
+const BACKEND_URL = window.location.hostname === 'localhost' ? 'http://localhost:6969' : 'https://poopoopants-api.now.sh/mews'
+
+
+list_all_lessons();
+
+function list_all_lessons(){
+    fetch('http://localhost:6969/lessons')
+        .then(response => response.json())
+        .then(lessons => {
+            //reset div
+            const lessons_div = document.getElementById('lessons');
+            lessons_div.innerHTML = "";
+            lessons.forEach(lesson => {
+
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = lesson.chapter_no + '.' + lesson.lesson_no;
+
+                const contents = document.createElement('p');
+                contents.textContent = lesson.details;
+
+                const notes = document.createElement('p');
+                notes.textContent = lesson.particles;
+                
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(notes);
+
+                lessons_div.appendChild(div);
+
+            })
+        })
+}
+
+const add_new_lesson_form = document.getElementById('add_new_lesson_form');
+add_new_lesson_form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log('form was submitted')
-    const form_data = new FormData(form);
-    const lesson = form_data.get('lesson');
+    const form_data = new FormData(add_new_lesson_form);
+
+    const chapter_no = form_data.get('chapter_no');
+    const lesson_no = form_data.get('lesson_no');
     const details = form_data.get('details');
+    const particles = form_data.get('particles')
 
     const ret = {
-        lesson, 
-        details
+        chapter_no,
+        lesson_no, 
+        details,
+        particles
     };
+    console.log(ret);
 
-    const api_url = 'http://localhost:6969/add_new_lesson';
-
-
-    fetch(api_url, {
+    fetch('http://localhost:6969/lessons', {
         method: 'POST',
         body: JSON.stringify(ret),
         headers: {
             'content-type': 'application/json'
         }
-    }).then(response => {
-            response.json();
-            console.log('hi');
-         })
-       .then(lesson => {
-           console.log('hi')
-           console.log(lesson);
-       });
+    }).then((response) => {
+        return response.json()
+    }).then(created_lesson => {
+        console.log(created_lesson);
+    });
+
+    list_all_lessons();
 });
--- server/index.js
+++ server/index.js
@@ -1,37 +1,60 @@
 const express = require('express');
 const cors = require('cors');
 const monk = require('monk');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 app.use(cors()); 
 app.use(express.json());
-
-const db = monk('localhost/poopoopants');
-const lessons = db.get('lessons');
-
+const db = monk(process.env.MONGO_URI || 'localhost:27017/poopoopants');
+db.then(() => {
+    console.log('Connected correctly to server')
+  })
 app.get('/', (req, res) => {
-    res.json({
-        message: "hi"
+    res.json("the poopoopants api");
+});
+
+app.get('/lessons', (req, res) => {
+    const lessons = db.get('lessons');
+    lessons
+        .find()
+        .then(lessons => {
+            res.json(lessons);
+        });
+});
+
+app.post('/lessons', (req, res) => {
+    const lesson = {
+        chapter_no: req.body.chapter_no.toString(),
+        lesson_no: req.body.lesson_no.toString(),
+        details: req.body.details.toString(),
+        particles: req.body.particles.toString(),
+        created: new Date()
+    };
+    
+   const lessons = db.get('lessons');
+   lessons.insert(lesson)
+        .then((created_lesson) => {
+            res.json(created_lesson) /*return the created lesson*/
+        }).catch((err) => {
+            // An error happened while inserting
+        });
+});
+
+app.put('/lessons', (req, res) => {
+
+})
+
+app.delete('/lessons', (req, res) => {
+    const lessons = db.get('lessons');
+    lessons.drop()
+    .then(lessons => {
+        res.json(lessons);
     });
 });
 
-app.post('/add_new_lesson', (req, res) => {
-    
-    const lesson = {
-        lesson_no: req.body.lesson.toString(),
-        details: req.body.details.toString(),
-        created: new Date()
-    };
-    console.log(lesson);
-    
-   /*const lesson = req.body;*/
-   lessons
-        .insert(lesson)
-        .then(created_lesson => {
-            console.log(created_lesson);
-            res.json(created_lesson);
-        });
-});
 
 app.listen(6969, () => {
     console.log('listening on 6969')
